a7f0fab1400074c38a368396752ed6ec66656ed0
Blake Simpson <blakersim@gmail.com>
2013-09-10 20:08:18 +0200

    Keep the keys out of the repo

--- data.rb
+++ data.rb
@@ -3,9 +3,8 @@
 require 'pry'
 require './src/xing.rb'
 require './src/shared_contacts.rb'
-
-KEY = '7ee45757712c345aff68'
-SECRET = '411a74eec535ce6b4631f49f0b278da9cee0fc4b'
+# Defined KEY and SECRET constants
+require './keys.rb'
 
 collector = SharedContacts.new
 collector.collect_data
